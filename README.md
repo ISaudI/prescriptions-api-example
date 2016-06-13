f2650240dcd5409766c705308a8940ab5fb4f4be
jordangarcia <jordan.garcia@gmail.com>
2016-06-13 15:11:57 -0700

    Initial commit
    Implement REST API for users, prescriptions, and fills

---
+++ README.md
@@ -0,0 +1,3 @@
+# Onsite Interview Programming Test
+
+We have included a dataset that contains an array of patients and their prescriptions. Create an API that serves this data to a UI that displays the information for a single patient at a time. Use whatever tools and technologies that you want.
---
+++ src/index.js
@@ -0,0 +1,74 @@
+const _ = require('lodash');
+const Hapi = require('hapi');
+const path = require('path');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+var DATA = {
+  users: require('../users.json'),
+  fills: require('../fills.json'),
+  prescriptions: require('../prescriptions.json'),
+}
+
+server.route({
+  method: 'GET',
+  path: '/api/users',
+  handler: function (request, reply) {
+    reply(DATA.users)
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/api/users/{id}',
+  handler: function (request, reply) {
+    var user = _.find(DATA.users, { id: Number(request.params.id) })
+    if (!user) {
+      reply().code(404);
+      return;
+    }
+    reply(user);
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/api/users/{user_id}/prescriptions',
+  handler: function (request, reply) {
+    var results = _.filter(DATA.prescriptions, (item) => item.user_id === Number(request.params.user_id));
+    reply(results);
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/api/prescriptions/{prescription_id}/fills',
+  handler: function (request, reply) {
+    var results = _.filter(DATA.fills, (item) => item.prescription_id === request.params.prescription_id);
+    reply(results);
+  }
+});
+
+server.register(require('inert'), (err) => {
+	if (err) {
+		throw err;
+	}
+
+	server.route({
+		method: 'GET',
+		path: '/',
+		handler: function (request, reply) {
+      console.log('in handler')
+      reply.file(path.join(__dirname, './index.html'));
+		}
+	});
+
+	server.start((err) => {
+		if (err) {
+			throw err;
+		}
+
+		console.log('Server running at:', server.info.uri);
+	});
+});
